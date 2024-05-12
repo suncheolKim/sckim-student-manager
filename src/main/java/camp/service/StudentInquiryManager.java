64fbb13f6efe8eb5d05d927610405eb19e91c19a
Suncheol Kim <nomail@no.mail>
2024-05-12 18:39:07 +0900

    feature - 등록된 수강생이 없는 케이스 메시지 출력 추가

--- src/main/java/camp/service/StudentInquiryManager.java
+++ src/main/java/camp/service/StudentInquiryManager.java
@@ -13,12 +13,17 @@ public class StudentInquiryManager {
 
     // 수강생 목록 조회
     public void inquireStudent() {
-        System.out.println("\n수강생 목록을 조회합니다...");
-        // 기능 구현
-        for (int i=0; i<studentStore.size(); i++) {
-            System.out.println("<<" + (i+1) + ">>\n" + studentStore.get(i));
+        if (studentStore.size() == 0) {
+            System.out.println("\n등록된 수강생이 없습니다...");
         }
+        else {
+            System.out.println("\n수강생 목록을 조회합니다...");
 
-        System.out.println("\n수강생 목록 조회 성공!");
+            for (int i = 0; i < studentStore.size(); i++) {
+                System.out.println("<<" + (i + 1) + ">>\n" + studentStore.get(i));
+            }
+
+            System.out.println("\n수강생 목록 조회 성공!");
+        }
     }
 }
